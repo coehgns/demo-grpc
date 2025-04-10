b8fc9cdd76830c0d7a949ced607a5fea82cf70bf
도훈 <modooboiroo@gmail.com>
2025-04-10 20:26:23 +0900

    docs: 주석 처리

--- src/main/java/com/example/demogrpc/domain/member/domain/Member.java
+++ src/main/java/com/example/demogrpc/domain/member/domain/Member.java
@@ -3,23 +3,23 @@ package com.example.demogrpc.domain.member.domain;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Getter
-@AllArgsConstructor
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Builder
-@Table(name = "tbl_member")
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @Column(name = "email", nullable = false, unique = true)
-    private String email;
-
-    @Column(name = "password", nullable = false)
-    private String password;
-}
+//@Entity
+//@Getter
+//@AllArgsConstructor
+//@NoArgsConstructor(access = AccessLevel.PROTECTED)
+//@Builder
+//@Table(name = "tbl_member")
+//public class Member {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//
+//    @Column(name = "name", nullable = false)
+//    private String name;
+//
+//    @Column(name = "email", nullable = false, unique = true)
+//    private String email;
+//
+//    @Column(name = "password", nullable = false)
+//    private String password;
+//}
--- src/main/java/com/example/demogrpc/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/com/example/demogrpc/domain/member/domain/repository/MemberRepository.java
@@ -1,7 +1,7 @@
-package com.example.demogrpc.domain.member.domain.repository;
-
-import com.example.demogrpc.domain.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
+//package com.example.demogrpc.domain.member.domain.repository;
+//
+//import com.example.demogrpc.domain.member.domain.Member;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface MemberRepository extends JpaRepository<Member, Long> {
+//}
