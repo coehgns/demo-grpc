7f17afecbb87a0cf0c7f2357631167a870fd08b8
도훈 <modooboiroo@gmail.com>
2025-04-10 23:09:29 +0900

    chore: init

--- src/main/java/com/example/demogrpc/domain/member/domain/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.example.demogrpc.domain.member.domain.repository;
-//
-//import com.example.demogrpc.domain.member.domain.Member;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface MemberRepository extends JpaRepository<Member, Long> {
-//}
